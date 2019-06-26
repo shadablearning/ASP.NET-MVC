7ed6c020dcd9c09563937e30d7d7c000854be2ed
shadablearning <51901767+shadablearning@users.noreply.github.com>
2019-06-26 13:34:58 +0530

    Initial commit

---
+++ MusicStore/MusicStoreWeb/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using MusicStoreWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreWeb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {            
+            return View();
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name="Shrek!"};
+            return View(movie);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ASP.NET MVC
+
+Create Sample APP using Template 
+
+MVC in Action >>  Create Movie Model and Pass it to Control 
+
+Action Results
