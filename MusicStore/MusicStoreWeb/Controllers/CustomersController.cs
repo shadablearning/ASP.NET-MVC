8a4ce8ac3e84c51ca1fa36cbe1cc2fb2cfc447c1
shadablearning <51901767+shadablearning@users.noreply.github.com>
2019-07-05 18:39:32 +0530

    Load Customer data without Lazy Loading. Query run Immediately

---
+++ MusicStore/MusicStoreWeb/Controllers/CustomersController.cs
@@ -0,0 +1,52 @@
+﻿using MusicStoreWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreWeb.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //var customers = GetCustomers();
+            //var customers = _context.Customers;//Deferred Execution
+
+            var customers = _context.Customers.ToList();//Query Run Immediately
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id); //Query Run immediately
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{ Id=1,Name="Employee1"},
+                new Customer{ Id=2,Name="Employee2"}
+            };
+        }
+    }
+}
