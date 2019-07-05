ee9a6113215d1f78c95b658edf8df47146bbe3d8
shadablearning <51901767+shadablearning@users.noreply.github.com>
2019-07-05 17:50:13 +0530

    ApplyAnnotationstoCustomerName

--- MusicStore/MusicStoreWeb/Models/Customer.cs
+++ MusicStore/MusicStoreWeb/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicStoreWeb.Models
 {
@@ -10,6 +11,8 @@ namespace MusicStoreWeb.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewLetter { get; set; }
